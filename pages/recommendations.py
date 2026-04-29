6dfd18a0ba3b69305b739b258bddd0456f705124
Neha Banjara <nehabanjara10@gmail.com>
2026-04-29 08:46:33 +0530

    fix: update API_BASE in recommendations page

--- pages/recommendations.py
+++ pages/recommendations.py
@@ -5,7 +5,7 @@ import streamlit as st
 # ==============================
 # CONFIG
 # ==============================
-API_BASE = os.getenv("API_BASE", "http://127.0.0.1:8000")
+API_BASE = os.getenv("API_BASE", "https://cinescope-movie-recommendation-system.onrender.com")
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(
